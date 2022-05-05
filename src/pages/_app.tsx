108147e14b2753dab7020243ef63556b4596383a
An Nguyen <ntan1@cmc.com.vn>
2022-05-05 19:21:59 +0700

    setup custom app with mui theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,33 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useApollo } from "../lib/apolloClient";
 import { ApolloProvider } from "@apollo/client";
+import Head from "next/head";
+import { NextPage } from "next";
+import React, { ReactElement, ReactNode } from "react";
+import ThemeProvider from "src/theme";
 
-const App = ({ Component, pageProps }: AppProps) => {
+type NextPageWithLayout = NextPage & {
+	getLayout?: (page: ReactElement) => ReactNode;
+};
+
+interface MyAppProps extends AppProps {
+	Component: NextPageWithLayout;
+}
+
+const App = ({ Component, pageProps }: MyAppProps) => {
 	const apolloClient = useApollo(pageProps);
 
+	const getLayout = Component.getLayout ?? ((page) => page);
+
 	return (
-		<ApolloProvider client={apolloClient}>
-			<Component {...pageProps} />
-		</ApolloProvider>
+		<>
+			<Head>
+				<meta name='viewport' content='initial-scale=1, width=device-width' />
+			</Head>
+			<ApolloProvider client={apolloClient}>
+				<ThemeProvider>{getLayout(<Component {...pageProps} />)}</ThemeProvider>
+			</ApolloProvider>
+		</>
 	);
 };
 
