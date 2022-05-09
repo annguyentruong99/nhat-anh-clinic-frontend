259c9d4c7cb8da639743f98e8310ea669089e1db
annguyentruong99 <an.nguyentruong99@gmail.com>
2022-05-09 14:10:45 +0700

    implement Module service slider to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
 import type { GetStaticProps, InferGetStaticPropsType } from "next";
 import { initializeApollo } from "src/lib/apolloClient";
-import { HomePageDocument } from "src/__generated__/types";
+import { HomePageDocument, ServicesDocument } from "src/__generated__/types";
 
 import ModuleHeroBanner from "src/modules/ModuleHeroBanner";
-import Banner from "public/assets/Banner.png";
+import ModuleServicesSlider from "src/modules/ModuleServicesSlider";
 
 const modulesMap: { [module: string]: string } = {
 	ModuleHeroBanner: "ComponentModulesHeroBanner",
+	ModuleServicesSlider: "ComponentModulesServicesSlider",
 };
 
-const Home = ({ pageData }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Home = ({
+	pageData,
+	services,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
 	const { modules } = pageData;
 
 	return (
@@ -21,6 +25,15 @@ const Home = ({ pageData }: InferGetStaticPropsType<typeof getStaticProps>) => {
 					)[0]
 				}
 			/>
+			<ModuleServicesSlider
+				moduleProps={
+					modules.filter(
+						(module) =>
+							module.__typename === modulesMap[ModuleServicesSlider.name],
+					)[0]
+				}
+				services={services}
+			/>
 		</div>
 	);
 };
@@ -31,9 +44,14 @@ export const getStaticProps: GetStaticProps = async () => {
 		.query({ query: HomePageDocument })
 		.then((res) => res);
 
+	const services = await apolloClient
+		.query({ query: ServicesDocument })
+		.then((res) => res);
+
 	return {
 		props: {
 			pageData: pageData?.data?.page?.data?.attributes,
+			services: services?.data?.services,
 		},
 	};
 };
