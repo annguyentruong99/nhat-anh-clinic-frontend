a2434ef58b0c52cbbedf7c5bd0f8bca656c32833
annguyentruong99 <an.nguyentruong99@gmail.com>
2022-05-09 13:41:21 +0700

    added service card component

---
+++ src/components/ServiceCard/styles.ts
@@ -0,0 +1,50 @@
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import List from "@mui/material/Grid";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+
+export const StyledCard = styled(Card)(({ theme }) => ({
+	width: "298px",
+	border: `1px solid ${theme.palette.grey[200]}`,
+}));
+
+export const StyledCardContent = styled(CardContent)({
+	padding: "24px 24px 0 24px",
+});
+
+export const StyledCardActions = styled(CardActions)({
+	padding: "32px 24px 24px 24px",
+});
+
+export const Index = styled(Typography)(({ theme }) => ({
+	color: theme.palette.grey[100],
+	fontWeight: 600,
+	marginBottom: "20px",
+}));
+
+export const ServiceName = styled(Typography)(({ theme }) => ({
+	fontWeight: 600,
+	color: theme.palette.primary.darker,
+}));
+
+export const Highlight = styled(Typography)(({ theme }) => ({
+	color: theme.palette.grey[500],
+}));
+
+export const StyledList = styled(List)(({ theme }) => ({
+	minHeight: "68px",
+	marginTop: "8px",
+}));
+
+export const StyledListItem = styled(ListItem)(({ theme }) => ({
+	padding: 0,
+}));
+
+export const StyledListItemIcon = styled(ListItemIcon)(({ theme }) => ({
+	minWidth: "fit-content",
+	marginRight: "4px",
+}));
