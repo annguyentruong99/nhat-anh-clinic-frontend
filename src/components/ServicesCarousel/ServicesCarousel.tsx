437588f6c8d0816d493a77e3cb8338551daa3205
annguyentruong99 <an.nguyentruong99@gmail.com>
2022-05-10 13:49:44 +0700

    added service carousel component

---
+++ src/components/ServicesCarousel/ServicesCarousel.tsx
@@ -0,0 +1,96 @@
+import { Swiper, SwiperProps, SwiperSlide } from "swiper/react";
+import { Scrollbar } from "swiper";
+
+import {
+	CarouselContainer,
+	navigationButtonStyles,
+	NavigationContainer,
+} from "./styles";
+import { HideInMobile } from "src/theme/common";
+import RightArrow from "../icons/RightArrow";
+import LeftArrow from "../icons/LeftArrow";
+import ServiceCard from "src/components/ServiceCard";
+
+import { SxProps, Theme, useMediaQuery, useTheme } from "@mui/material";
+import Box from "@mui/material/Box";
+import { useEffect, useState } from "react";
+import { Services } from "src/typings/services.types";
+
+interface Props {
+	services: Services;
+}
+
+const Carousel: React.FC<Props> = ({ services: { data: servicesData } }) => {
+	const theme = useTheme();
+	const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
+
+	const [swiper, setSwiper] = useState(null);
+
+	useEffect(() => {
+		setSwiper(document.querySelector(".services-carousel")?.swiper);
+	}, []);
+
+	const paginationStyles: SxProps<Theme> = {
+		"& .swiper": {
+			height: "565px",
+
+			"& .swiper-scrollbar": {
+				width: "88px",
+				backgroundColor: (theme) => theme.palette.grey[100],
+				left: "50%",
+				transform: "translate(-50%, -50%)",
+
+				"& .swiper-scrollbar-drag": {
+					width: `${88 / servicesData.length}px`,
+					backgroundColor: "primary.darker",
+				},
+			},
+		},
+	};
+
+	const swiperProps: SwiperProps = {
+		height: 575,
+		slidesPerView: isDesktop ? 3 : 1,
+		spaceBetween: isDesktop ? 100 : 50,
+		initialSlide: isDesktop ? 2 : 0,
+		watchOverflow: true,
+		modules: [Scrollbar],
+		scrollbar: {
+			draggable: true,
+			hide: false,
+			snapOnRelease: true,
+		},
+		className: "services-carousel",
+	};
+
+	return (
+		<CarouselContainer
+			sx={{
+				...paginationStyles,
+			}}>
+			<Swiper {...swiperProps}>
+				{servicesData.map((service, ind) => (
+					<SwiperSlide key={service.attributes.slug}>
+						<ServiceCard service={service} index={ind + 1} />
+					</SwiperSlide>
+				))}
+			</Swiper>
+			<HideInMobile>
+				<NavigationContainer>
+					<Box
+						sx={{ ...navigationButtonStyles }}
+						onClick={() => swiper.slidePrev()}>
+						<LeftArrow />
+					</Box>
+					<Box
+						sx={{ ...navigationButtonStyles }}
+						onClick={() => swiper.slideNext()}>
+						<RightArrow />
+					</Box>
+				</NavigationContainer>
+			</HideInMobile>
+		</CarouselContainer>
+	);
+};
+
+export default Carousel;
---
+++ src/components/ServicesCarousel/styles.ts
@@ -0,0 +1,42 @@
+import { styled } from "@mui/material/styles";
+import { SxProps, Theme } from "@mui/material";
+
+export const CarouselContainer = styled("div")(({ theme }) => ({
+	marginTop: "24px",
+
+	[theme.breakpoints.up("md")]: {
+		marginTop: "16px",
+	},
+}));
+
+export const NavigationContainer = styled("div")(({ theme }) => ({
+	display: "flex",
+	position: "absolute",
+	bottom: "10%",
+	left: "90%",
+	zIndex: 100,
+}));
+
+export const navigationButtonStyles: SxProps<Theme> = {
+	width: "44px",
+	height: "44px",
+	border: (theme) => `1px solid ${theme.palette.grey[500]}`,
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+	cursor: "pointer",
+	marginRight: "8px",
+
+	"&:hover": {
+		backgroundColor: (theme) => theme.palette.primary.darker,
+		border: (theme) => `1px solid ${theme.palette.primary.darker}`,
+		transition: "background-color 0.3s",
+		"& svg": {
+			fill: (theme) => theme.palette.grey[0],
+		},
+	},
+
+	"& svg": {
+		fill: (theme) => theme.palette.grey[500],
+	},
+};
