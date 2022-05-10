aa58382e1da0a438e8f0c617785504c17ba7ea43
annguyentruong99 <an.nguyentruong99@gmail.com>
2022-05-11 01:13:59 +0700

    added DoctorCard component

---
+++ src/components/DoctorCard/styles.ts
@@ -0,0 +1,44 @@
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+
+export const StyledCard = styled(Card)(({ theme }) => ({
+	display: "block",
+	border: `1px solid ${theme.palette.grey[200]}`,
+	width: "300px",
+	height: "550px",
+
+	[theme.breakpoints.up("md")]: {
+		width: "405px",
+	},
+}));
+
+export const StyledCardContent = styled(CardContent)(({ theme }) => ({
+	padding: "24px 24px 0 24px",
+	height: "fit-content",
+}));
+
+export const StyledCardActions = styled(CardActions)(({ theme }) => ({
+	padding: "10px 24px 24px 24px",
+	width: "100%",
+
+	[theme.breakpoints.up("md")]: {
+		width: "50%",
+	},
+}));
+
+export const Role = styled(Typography)(({ theme }) => ({
+	color: theme.palette.grey[500],
+}));
+
+export const Name = styled(Typography)(({ theme }) => ({
+	textTransform: "uppercase",
+	color: theme.palette.primary.darker,
+	margin: "16px 0 8px 0",
+}));
+
+export const Description = styled(Typography)(({ theme }) => ({
+	color: theme.palette.grey[500],
+}));
