ef50ebaa98477da5dc6217afcecd78d5b97f61cb
annguyentruong99 <an.nguyentruong99@gmail.com>
2022-05-19 23:17:42 +0700

    added customized Tabs and Tab component

--- src/theme/overrides/index.ts
+++ src/theme/overrides/index.ts
@@ -1,9 +1,13 @@
 import { MuiButton } from "./MuiButton";
 import { MuiAppBar } from "./MuiAppBar";
+import { MuiTab } from "./MuiTab";
+import { MuiTabs } from "./MuiTabs";
 
 const components = {
 	MuiButton,
 	MuiAppBar,
+	MuiTabs,
+	MuiTab,
 };
 
 export default components;
