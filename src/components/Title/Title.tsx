b69f7dc6198e79d30394c8c1ae783fd8f15d8f66
annguyentruong99 <an.nguyentruong99@gmail.com>
2022-05-25 01:03:15 +0700

    added sx props to title field

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,18 +1,26 @@
 import { Title1, Title2 } from "./styles";
 
 import { Variant } from "@mui/material/styles/createTypography";
+import { SxProps } from "@mui/material/styles";
 
 interface Props {
 	title1: string;
 	title2?: string;
 	variant?: Variant;
+	sx?: SxProps;
 }
 
-const Title: React.FC<Props> = ({ title1, title2, variant = "h3" }) => {
+const Title: React.FC<Props> = ({ title1, title2, variant = "h3", sx }) => {
 	return (
 		<>
-			<Title1 variant={variant}>{title1 + " "}</Title1>
-			{Boolean(title2) && <Title2 variant={variant}>{title2}</Title2>}
+			<Title1 variant={variant} sx={sx}>
+				{title1 + " "}
+			</Title1>
+			{Boolean(title2) && (
+				<Title2 variant={variant} sx={sx}>
+					{title2}
+				</Title2>
+			)}
 		</>
 	);
 };
